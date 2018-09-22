abad902315acc7ca23c8bd8ae5cb2277c8b96cdb
matheusvds <matheusvds@users.noreply.github.com>
2018-09-22 15:16:46 -0300

    Creates base folder structure

---
+++ Weather/Shared/Essentials/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Matheus Vasconcelos on 22/09/18.
+//  Copyright © 2018 Matheus Sousa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
