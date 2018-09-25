751704ead7edc848a79f6cd3c987d22388ab6b8e
matheusvds <matheusvds@users.noreply.github.com>
2018-09-25 10:16:08 -0300

    Creates a separated datasource for the table

---
+++ Weather/Forecast/Datasource/ForecastTableViewDatasource.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastTableViewDatasource.swift
+//  Weather
+//
+//  Created by Matheus Vasconcelos on 25/09/18.
+//  Copyright © 2018 Matheus Sousa. All rights reserved.
+//
+
+import Foundation
+
+class ForecastTableViewDatasource: NSObject {
+    
+}
