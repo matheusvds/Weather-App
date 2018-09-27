334c777d693cd5e6b8a8124a475a16fb1ef840d9
matheusvds <matheusvds@users.noreply.github.com>
2018-09-27 00:14:11 -0300

    Configures a basic separated datasource for ForecastTableView

--- Weather/Forecast/Controller/ForecastViewController.swift
+++ Weather/Forecast/Controller/ForecastViewController.swift
@@ -7,11 +7,68 @@
 //
 
 import UIKit
+import Moya
 
 class ForecastViewController: UIViewController {
 
-    override func loadView() {
-        self.view = UIView()
+    private var state: State<[Weather]> = .loading {
+        didSet {
+            switch state {
+            case .ready(let forecast):
+                self.forecastDatasource?.reloadData(with: forecast)
+            case .loading:
+                break
+            case .error(let error):
+                print(error)
+                break
+            }
+        }
+    }
+   
+    var provider: MoyaProvider<OpenWeatherMap>
+    let forecastView = ForecastViewControllerScreen()
+    var forecastDatasource: ForecastTableViewDatasource?
+    
+    init(provider: MoyaProvider<OpenWeatherMap> = MoyaProvider<OpenWeatherMap>()) {
+        self.provider = provider
+        super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        self.view = forecastView
+        self.state = .loading
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        requestData(with: (lat: "37,332331", lon: "-122,406417"))
+    }
+    
+    func setup() {
+        self.forecastDatasource = ForecastTableViewDatasource(forecast: [],
+                                                              tableView: self.forecastView.tableView)
+    }
+    
+    func requestData(with location: Location) {
+        provider.request(.forecast(location)) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let response):
+                do {
+                    let response = try response.map(Forecast.self)
+                    self.state = .ready(response.list)
+                } catch(let error) {
+                    print(error)
+                    self.state = .error(.unkown)
+                }
+            case .failure(let error):
+                self.state = .error(WeatherError(error: error))
+            }
+        }
+    }
 }
--- Weather/Forecast/Datasource/ForecastTableViewDatasource.swift
+++ Weather/Forecast/Datasource/ForecastTableViewDatasource.swift
@@ -6,8 +6,36 @@
 //  Copyright © 2018 Matheus Sousa. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class ForecastTableViewDatasource: NSObject {
     
+    var data: [Weather]
+    var tableView: UITableView
+    
+    init(forecast: [Weather],
+         tableView: UITableView) {
+        self.data = forecast
+        self.tableView = tableView
+        super.init()
+        self.tableView.dataSource = self
+    }
+    
+    func reloadData(with forecast: [Weather]) {
+        self.data = forecast
+        self.tableView.reloadData()
+    }
+}
+
+extension ForecastTableViewDatasource: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
 }
